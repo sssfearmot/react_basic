87f499d725dcf189e99e75f3bd17f8ef6898ea80
IMMJ <story_mania@naver.com>
2022-07-25 12:23:59 +0900

    2022-07-25

---
+++ test/src/App.js
@@ -0,0 +1,15 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  return (
+    <>
+    <Test />
+    <h1>호호호</h1>
+    <Nav />
+    <Test />
+    <Test />
+    </>
+  )
+}
+export default App;
